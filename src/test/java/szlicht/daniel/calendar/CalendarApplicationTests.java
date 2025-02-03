42e8ce322eec08d8428b2eb706450319278f4806
Daniel Szlicht <daniel.szlicht25@gmail.com>
2025-02-03 19:31:12 +0100

    Initial commit

---
+++ src/test/java/szlicht/daniel/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package szlicht.daniel.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
