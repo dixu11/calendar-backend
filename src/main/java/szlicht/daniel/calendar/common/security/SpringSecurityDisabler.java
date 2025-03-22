6fa43439deb08578a3101d2b50ccfbf51c30c318
Daniel Szlicht <daniel.szlicht25@gmail.com>
2025-03-22 09:11:58 +0100

    finish refactoring mentoring offer scenario fixes #152

--- src/main/java/szlicht/daniel/calendar/common/security/SpringSecurityDisabler.java
+++ src/main/java/szlicht/daniel/calendar/common/security/SpringSecurityDisabler.java
@@ -5,25 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import szlicht.daniel.calendar.meeting.Logger;
 
 @Configuration //comment to disable
 public class SpringSecurityDisabler {
-    private int counter = 0;
-    private Logger logger;
-
-    public SpringSecurityDisabler(Logger logger) {
-        this.logger = logger;
-    }
 
     @Bean
     SecurityFilterChain configureChain(HttpSecurity http) throws Exception {
-        counter++;
-        if (counter > 20) {
-            logger.notifyOwner("so many new students? Probably attack. Disabling app", "", true);
-        }
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(customizer -> customizer.anyRequest().permitAll())
                 .build();
     }
